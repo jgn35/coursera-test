5b10f1ceb2a45d596f43a2bf618b6f985832349b
Jean-Guilhem Nousse <jean-guilhem.nousse@orange.com>
2025-09-09 15:40:19 +0200

    Add color bonsu

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -6,11 +6,14 @@
     lunchCheck.$inject = ["$scope"];
     function lunchCheck($scope) {
         $scope.menu = "";
+        $scope.data=""
         $scope.checkIfTooMuch = function () {
             if ($scope.menu == "") {
                 $scope.message = "Please enter data first";
+                $scope.messageClass = "no-data";
                 return;
             }
+            $scope.messageClass = "has-data";
             var items = $scope.menu.split(',');
             if (items.length <= 3) {
                 $scope.message = "Enjoy!";
